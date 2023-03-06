2a8151e8943448bfc90af23a7482c957d2d064e0
hoangc04 <hoangc04@uw.edu>
2023-03-06 15:33:32 -0800

    Saved Changes

--- app.R
+++ app.R
@@ -10,7 +10,16 @@ ui <- fluidPage(
     
     mainPanel(
       tabsetPanel(
-        tabPanel("Overview"),
+        tabPanel("Overview",
+                 p("\nThis app describes the results of data collected from a survey conducted to 
+                   examine academic variables in comparison to topics of mental health. The 
+                   public dataset is titled 'Student Mental Health', and is defined as a 
+                   statistical research on the effects of mental health on students' CGPA.\n"),
+                 p("\nThis data was collected through a survey catered primarily and solely 
+                   to university students. It aims to recognize the correlation or possible 
+                   association between academic outcomes and variables of mental heatlh.\n"),
+                 p("\nThe following are several samples of observations within the dataset.\n"),
+                 tableOutput("sample")),
         
         tabPanel("Plot"),
         
@@ -23,7 +32,12 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-
+  
+  output$sample <- renderTable({
+    health %>% 
+      sample_n(5)
+  })
+  
 }
 
 
