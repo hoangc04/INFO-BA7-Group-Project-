352594e3f43a62df332446ee3984340dc5acb039
hoangc04 <hoangc04@uw.edu>
2023-03-06 20:52:23 -0800

    Saved Changes

--- app.R
+++ app.R
@@ -6,20 +6,28 @@ health <- read.csv("student-mental-health.csv")
 
 ui <- fluidPage(
 
-    titlePanel("BA7 Group Project"),
+    titlePanel("Student Mental Health"),
     
     mainPanel(
       tabsetPanel(
         tabPanel("Overview",
+                 headerPanel("General Overview"),
                  p("\nThis app describes the results of data collected from a survey conducted to 
                    examine academic variables in comparison to topics of mental health. The 
                    public dataset is titled 'Student Mental Health', and is defined as a 
                    statistical research on the effects of mental health on students' CGPA.\n"),
-                 p("\nThis data was collected through a survey catered primarily and solely 
-                   to university students. It aims to recognize the correlation or possible 
-                   association between academic outcomes and variables of mental heatlh.\n"),
-                 p("\nThe following are several samples of observations within the dataset.\n"),
-                 tableOutput("sample")),
+                 headerPanel("Data"),
+                 p("\nAs explained, we are working with a public data set published on Kaggle. 
+                   data set contains 101 observations, and 11 variables, 9 variables of which are
+                    relevant to out analysis and will be addressing. Variables will include demographics,
+                     academic situation, and several mental health topics. These observations were 
+                     collected in 2020, conducted as an online survey primarily and solely catered
+                     towards university students.\n"),
+                 headerPanel("Purpose"),
+                 p("\nThe purpose of examining this data set is to find an association between academic 
+                   situation and state of mental health. As the data has been collected from 
+                   university students, they will be our main audience to address. However, the findings 
+                   from this report can also apply to other audiences in current academic situations.\n")),
         
         tabPanel("Plot"),
         
